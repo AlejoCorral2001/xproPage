cc435744410f84f78aef92f7b2069f2e11153cf3
wilitp <guilledeipola@gmail.com>
2020-12-19 14:55:31 -0300

    Remove Plugin Offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,48 +1,46 @@
-const dotenv  = require('dotenv')
-if(process.env.NODE_ENV != 'production'){
-  dotenv.config()
-}else{
-  dotenv.config()
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV != 'production') {
+  dotenv.config();
+} else {
+  dotenv.config();
 }
 
 module.exports = {
   plugins: [
-  
-    `gatsby-transformer-sharp`, 
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-fontawesome-css`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/static/`,
+        path: `${__dirname}/src/static/`
       }
     },
     {
       resolve: 'gatsby-source-contentful',
-      options:{
+      options: {
         spaceId: process.env.SPACE_ID,
-        accessToken: process.env.ACCESS_TOKEN,
+        accessToken: process.env.ACCESS_TOKEN
       }
-    },{
+    },
+    {
       resolve: `gatsby-theme-animated-tailwind`,
       options: {
         tailwindConfig: `tailwind.config.js`
       }
-    }, 
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `src/logo/logo2.png`, // This path is relative to the root of the site.
-      },
-    },`gatsby-plugin-offline`,
-    
+    }
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `GatsbyJS`,
+    //     short_name: `GatsbyJS`,
+    //     start_url: `/`,
+    //     background_color: `#f7f0eb`,
+    //     theme_color: `#a2466c`,
+    //     display: `standalone`,
+    //     icon: `src/logo/logo2.png`, // This path is relative to the root of the site.
+    //   },
+    // },`gatsby-plugin-offline`,
   ]
-  
 };
