1d2fd3d579fcf45264a12d7a85327d947f59b3c4
AlejoCorral <corralex66@gmail.com>
2021-07-29 11:48:27 -0300

    se agrego el tema de la compra de fresas mediante wsp

--- src/components/productos/fresa.js
+++ src/components/productos/fresa.js
@@ -18,12 +18,17 @@ if(width<500){
 }
 
 
-
+const carrito = props.cantidad
 const extra = props.size?(
     <>
     <div className='text-center p-2 font-medium border-gray-900  m-6 text-lg '>{node.description.description}</div>
     <div className='text-center p-2 font-medium border-gray-900  m-6 text-xl'>{node.price}</div>
     <div className='text-center p-2 font-medium border-gray-900  m-6 font-serif text-lg mx-auto rounded-lg' style={{width:'fit-content',backgroundColor:node.available==true?'#48bb78':'#fc8181'}}>{node.available==true?'disponible':'agotado'}</div>
+    <div className='flex  justify-center mx-auto bg-gray-400 rounded-lg' style={{width:"fit-content"}}>
+        <div className='font-bold text-4xl md:text:6x1 px-3 sm:px-4 md:px-6 lg:px-8 text-primary-darker' onClick={props.remove}>-</div>
+        <div className='font-bold text-4xl md:text:6x1 px-3 sm:px-4 md:px-6 lg:px-8 text-primary-lighter'>{carrito}</div>
+        <div className='font-bold text-4xl md:text:6x1 px-3 sm:px-4 md:px-6 lg:px-8 text-primary-darker' onClick={props.add}>+</div>
+    </div>
     </>
 ):
 (
@@ -35,6 +40,7 @@ return(
     <div onClick={props.func} className={Style.transition+" hover:shadow-xl bg-orange-300 m-2 p-2 md:m-3 md:p-4  rounded-lg"} style={{width:size, backgroundColor: props.size?'#f6ad55':'#fbd38d'}} >
           
             <p className="font-semibold text-sm text-center mb-4 h-12  sm:text-xl">{node.name}</p>
+            
           
             <Img className='rounded-lg w-full' fluid={node.picture.fluid}/>
             {extra}
--- src/components/productos/fresas.js
+++ src/components/productos/fresas.js
@@ -32,6 +32,7 @@ query{
 
 `)
 
+
 const [bigOne, setBigOne] = useState(0);
 const enlarge = (index) => {
   setBigOne(index+1)
@@ -39,13 +40,55 @@ const enlarge = (index) => {
 const fresas = data.allContentfulFresas.nodes
 let result=0;
 
+let cantidadesGenerator = []
+for (let i=0; i<fresas.length; i++){
+  cantidadesGenerator[i]=0
+}
+const [cantidadesEstado, setCantidadesEstado] = useState(cantidadesGenerator)
+let cantidades = cantidadesEstado
 
+const [actualizar, setActualizar] = useState(0)
+
+const add = (index) =>{
+  let arr = cantidadesEstado
+  arr[index] = arr[index]+1
+  setCantidadesEstado(arr)
+  setActualizar(!actualizar)
+
+}
+const remove = (index) =>{
+  let arr = cantidadesEstado
+  arr[index] = arr[index]>0?arr[index]-1:0
+  setCantidadesEstado(arr)
+  setActualizar(!actualizar)
+  }
+
+const comprar = () => {
+  let msg = ""
+  let total=0
+  for (let i=0; i<fresas.length; i++){
+    if(cantidadesEstado[i]!=0){
+      msg=msg+("--"+cantidadesEstado[i]+"x "+fresas[i].name+"|  ")
+      total = total + parseInt(fresas[i].price.substr(1, fresas[i].price.length)) * cantidadesEstado[i]
+    }
+    
+  }
+ msg=msg+"El precio total es de: $"+total
+ msg="https://api.whatsapp.com/send/?phone=5493517889350&text="+msg
+ window.open(msg ,"_top")
+
+}
+  
 return(
+  <>
     <div className="container mx-auto px-2 flex flex-wrap justify-center"> 
   {fresas.map((node, index)=>(
-    <Fresa obj={node} size={bigOne==index+1?true:false} func={() => {enlarge(index)}}/>
+    <Fresa obj={node} size={bigOne==index+1?true:false} add={()=>add(index)} remove={()=>remove(index)} func={() => {enlarge(index)}} cantidad={cantidadesEstado[index]}/>
   ))}
     </div>
+
+    <div className='flex justify-center bg-gray-400 rounded-lg mx-auto p-6 mt-12' style={{width:"fit-content"}} onClick={comprar}> <p className='text-3xl md:text-5xl text-primary'>COMPRAR</p></div>
+    </>
     
 )
 }
