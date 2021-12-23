438065086705d616d570b0510040675a599127f3
AlejoCorral <corralex66@gmail.com>
2021-12-23 11:43:37 -0300

    se soluciono padding y margin de los precios de las maquinas

--- src/components/productos/products.js
+++ src/components/productos/products.js
@@ -57,8 +57,8 @@ return(
             <div >
                 <Img className={'rounded-lg'}fluid={node.images[0].fluid}/>
             </div>
-            <p className="text-center mt-8 font-bold text-primary text-lg md:text-2xl mx-auto p-5 rounded-lg" style={{width:"fit-content"}}>{node.price}</p>
-            <p className="text-center mt-8 font-bold text-primary-darker text-lg bg-gray-400 mx-auto p-5 rounded-lg" style={{width:"fit-content"}}>Mas Información</p>
+            <p className="text-center mt-4 font-bold text-primary text-lg md:text-2xl mx-auto p-5 rounded-lg" style={{width:"fit-content"}}>{node.price}</p>
+            <p className="text-center mt-4 font-bold text-primary-darker text-lg bg-gray-400 mx-auto p-4 rounded-lg" style={{width:"fit-content"}}>Mas Información</p>
             
             </Card>
             </Link>
