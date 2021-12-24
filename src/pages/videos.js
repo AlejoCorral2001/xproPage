7416269be33b9e3fab3b08763a59e0a276208a43
AlejoCorral <corralex66@gmail.com>
2021-12-24 19:01:51 -0300

    se arreglo el ancho de los videos

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -15,7 +15,7 @@ export default () => (
   <Layout  type={'videos'}>
     <div className="mx-auto px-4 md:mx-24">
     <section className="pt-20">
-    <div className="mx-auto" style={{width:"75%",height:"75vh"}}>
+    <div className="mx-auto" style={{height:"75vh"}}>
     <iframe className='rounded-lg' style={{width:"100%",height:"100%"}} src="https://www.youtube.com/embed/jOJkZgz5i9Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe>
     </div>
     </section>
